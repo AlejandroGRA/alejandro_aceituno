88df3e65fb86b39614a507499134bb03e61c3955
Alejandro Aceituno <alejandro.gaceituno@hotmail.com>
2025-11-27 20:27:20 +0100

    Changing the email color to white

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header: React.FC<HeaderProps> = ({
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
-              <a href={`mailto:${email}`} className="hover:text-primary-200 transition-colors">
+              <a href={`mailto:${email}`} className="text-white hover:text-primary-200 transition-colors">
                 {email}
               </a>
             </div>
